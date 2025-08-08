e8e997c54bc520c6139af7ef5853e7e45b4845c2
Isaac <yisehakmitiku@gmail.com>
2025-08-08 15:51:28 -0700

    feat: add NavBar component with scroll effect and mode toggle

---
+++ src/components/ui/nav-bar.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { ModeToggle } from '../mode-toggle';
+import { cn } from '@/lib/utils';
+
+
+export default function NavBar() {
+
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+
+  return (
+    <nav className={cn('fixed top-0 w-full transition-all duration-500', scrollY > 100 && 'backdrop-blur-xl bg-background/30 border-b-border border-[1px]')}>
+      <div className='max-w-5xl flex justify-between items-center mx-auto px-8 py-6'>
+        <h1 className='text-lg font-medium'>Wes Isaac</h1>
+        <div className='flex justify-center items-center gap-20'>
+
+          <ul className='flex justify-center items-center gap-5 text-sm text-muted-foreground '>
+            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>Home</li>
+            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>About</li>
+            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>Contact</li>
+          </ul>
+          <ModeToggle />
+        </div>
+
+
+      </div>
+
+    </nav>
+  )
+}
