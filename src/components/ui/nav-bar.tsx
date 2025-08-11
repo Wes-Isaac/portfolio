53c7b53333511d856e6dceeb0d8454f55b2569ed
Isaac <yisehakmitiku@gmail.com>
2025-08-11 14:40:59 -0700

    feat: update NavBar component with responsive links and smooth scrolling

--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -20,17 +20,25 @@ export default function NavBar() {
     <nav className={cn('fixed top-0 w-full transition-all duration-500', scrollY > 70 && 'backdrop-blur-2xl bg-background/30 border-b-border border-[1px]')}>
       <div className='max-w-5xl flex justify-between items-center mx-auto px-8 py-6'>
         <h2 className='text-lg font-medium'>Wes Isaac</h2>
-        <div className='flex justify-center items-center gap-20'>
+        <div className='hidden md:flex justify-center items-center gap-20'>
 
-          <ul className='flex justify-center items-center gap-5 text-sm text-muted-foreground '>
-            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>Home</li>
-            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>About</li>
-            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>Contact</li>
+          <ul className='hidden md:flex justify-center items-center gap-5 text-sm text-muted-foreground '>
+            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>
+              <a href="#about">
+                About
+              </a>
+            </li>
+            <li className='hover:text-foreground hover:cursor-pointer transition-colors duration-300'>
+              <a href="#contact">
+                Contact
+              </a>
+            </li>
           </ul>
           <ModeToggle />
         </div>
-
-
+        <div className='md:hidden flex items-center gap-4'>
+          <ModeToggle />
+        </div>
       </div>
 
     </nav>
